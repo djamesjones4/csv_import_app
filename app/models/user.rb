a338f32ccc8618b2e5645d66b1a4a3e70a59bc79
Derek Jones <djamesjones4@gmail.com>
2025-02-07 21:15:02 -0800

    annotate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  first_name :string
+#  last_name  :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
 end
