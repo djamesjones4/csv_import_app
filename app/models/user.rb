7533a2f6c098250eb2e72ad9a705c8e15a44f6e0
Derek Jones <djamesjones4@gmail.com>
2025-02-08 22:50:07 -0800

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+  validates_presence_of :first_name, :last_name, :email
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
